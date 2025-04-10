70d92eff8ecd561ebd362dc9a15f526c09e6f369
VitaeDNA <info@vitaedna.com>
2025-04-10 18:45:50 +0400

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,1 +1,17 @@
-[CODICE UTILS/DATABASE.JS]
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const messageSchema = new mongoose.Schema({
+  from: String,
+  message: String,
+  response: String,
+  timestamp: { type: Date, default: Date.now }
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
