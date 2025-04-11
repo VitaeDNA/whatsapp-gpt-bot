b69717eb940506d861a140632743c88575062c8e
VitaeDNA <info@vitaedna.com>
2025-04-11 14:00:13 +0400

    Update server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.post('/webhook', async (req, res) => {
 async function downloadMedia(mediaId, mediaType) {
   const url = `https://graph.facebook.com/v18.0/${mediaId}`;
   const headers = {
-    Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`
+    Authorization: `Bearer ${process.env.META_ACCESS_TOKEN}`
   };
 
   const response = await axios.get(url, { headers });
@@ -107,7 +107,7 @@ async function sendWhatsAppMessage(to, text) {
     },
     {
       headers: {
-        Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+        Authorization: `Bearer ${process.env.META_ACCESS_TOKEN}`,
         'Content-Type': 'application/json'
       }
     }
